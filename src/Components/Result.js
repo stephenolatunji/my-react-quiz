9285b8ae356b592ca55ea22d0e1bc4689daf43e5
Stephen Olatunji <stephenolatunji02@gmail.com>
2019-12-26 10:48:57 +0100

    first commit on branch

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Button } from 'semantic-ui-react';
 
 
 function Result(props) {
@@ -6,30 +7,28 @@ function Result(props) {
     if (props.scorePoint < 5) {
         return (
             <div>
-                <div className="card-body">
-                    <span> Awful, Your Score is</span><br />
-                    {props.scorePoint}/10 !
-                </div>
+                <span> Awful, Your Score is</span><br />
+                {props.scorePoint}/10 !
             </div>
-            
+    
         )
     }
 
     else if (props.scorePoint < 8) {
         return (
-                <div className="card-body">
-                    <span> Nice Try, Your Score is</span><br />
-                    {props.scorePoint}/10 !
-                </div>
+            <div>
+                <span> Nice Try, Your Score is</span><br />
+                {props.scorePoint}/10 !
+            </div>
             
         )
     }
     else {
         return (
-                <div className="card-body">
-                    <span> Congrats, Your Score is</span><br />
-                    {props.scorePoint}/10 !
-                </div>
+            <div>
+                <span> Congrats, Your Score is</span><br />
+                {props.scorePoint}/10 !
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import MyQuiz from './MyQuiz'
 import "./styles.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 ReactDom.render(<MyQuiz />, document.getElementById('root'))
